32b945dbe8cc7c0686542dbba6d37563dd30f7bf
reuben-alt <rubenhattab@gmail.com>
2020-05-20 17:58:38 +0200

    Ajout appel serveur

---
+++ app/src/main/java/com/example/pokemon/PokeApi.java
@@ -0,0 +1,10 @@
+package com.example.pokemon;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface PokeApi {
+
+    @GET("/api/v2/pokemon")
+    Call<RestPokemonResponse> getPokemonResponse();
+}
---
+++ app/src/main/java/com/example/pokemon/Pokemon.java
@@ -0,0 +1,6 @@
+package com.example.pokemon;
+
+public class Pokemon {
+
+
+}
